d80b99287ab8c2c71cbb26777e5c8c644162a4c9
Ferrel John Fernando <f.syntaxer@gmail.com>
2024-08-01 20:14:54 +0700

    A. 29 - 31

---
+++ golang-week-8/go.mod
@@ -0,0 +1,3 @@
+module golang-week-8
+
+go 1.22.4
---
+++ golang-week-8/student/student.go
@@ -0,0 +1,32 @@
+package student
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Student struct {
+	Name  string
+	Grade int
+}
+
+func (s *Student) GetPropertyInfo() {
+	var reflectValue = reflect.ValueOf(s)
+
+	if reflectValue.Kind() == reflect.Ptr {
+		reflectValue = reflectValue.Elem()
+	}
+
+	var reflectType = reflectValue.Type()
+
+	for i := 0; i < reflectValue.NumField(); i++ {
+		fmt.Println("nama      :", reflectType.Field(i).Name)
+		fmt.Println("tipe data :", reflectType.Field(i).Type)
+		fmt.Println("nilai     :", reflectValue.Field(i).Interface())
+		fmt.Println("")
+	}
+}
+
+func (s *Student) SetName(name string) {
+	s.Name = name
+}
